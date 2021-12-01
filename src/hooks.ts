1f1d06637d9ba550e022bc6e93055e712e2c8af6
Ole Umlauft <ole.umlauft@protonmail.com>
2021-12-01 16:26:25 +0100

    Check for mobile phones only

---
+++ src/hooks.ts
@@ -0,0 +1,5 @@
+export function getSession(request) {
+  return {
+    userAgent: request.headers['user-agent']
+  };
+}
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,17 @@
+<script context="module">
+  export async function load({ session }) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(session.userAgent)) {
+      return {
+        status: 200
+      };
+    }
+    return {
+      status: 400,
+      error: 'You have to use this website on a mobile phone.'
+    };
+  }
+</script>
+
 <script>
   import '../app.css';
 </script>
