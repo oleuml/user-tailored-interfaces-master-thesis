5a1e5c158432a5518436640715d5a82410ca8bd5
Ole Umlauft <ole.umlauft@protonmail.com>
2021-12-21 12:02:50 +0100

    Updates layout

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script>
-  import { mdiAlert } from '@mdi/js';
+  import Button from '$lib/material/Button.svelte';
+  import TopBar from '$lib/material/TopBar.svelte';
+
+  import { mdiAlert, mdiOpenInNew } from '@mdi/js';
   import Icon from 'mdi-svelte';
   async function start() {
     return await fetch('/api/start', {
@@ -18,16 +21,26 @@
   let checked = false;
 </script>
 
-<div class="box text-blue-600 bg-blue-200">
+<div class="flex flex-col h-screen">
+  <div class="flex-initial">
+    <TopBar title="Studie">
+      <div slot="start" />
+      <div slot="end" />
+    </TopBar>
+  </div>
+  <div class="flex-auto">
+    <div class="grid grid-flow-row grid-cols-1 gap-2 mx-2 mt-2 select-none">
+      <div class="box text-blue-900 bg-blue-100">
   <article class="p-2">
     <p class="font-bold pb-1">Hallo und herzlich willkommen!</p>
     <p>
-      Vielen Dank erstmal, dass Du den Weg hierher gefunden hast. Im Rahmen meiner Masterarbeit
-      führe ich diese Studie durch. Die Studie dauert durchschnittlich ca. 15 min.
+            Vielen Dank erstmal, dass Du den Weg hierher gefunden hast. Im Rahmen meiner
+            Masterarbeit führe ich diese Studie durch. Die Studie dauert durchschnittlich ca. 15
+            min.
     </p>
   </article>
 </div>
-<div class="box text-red-600 bg-red-200">
+      <div class="box text-red-700 bg-red-200">
   <article class="p-2">
     <p class="font-bold pb-1 flex">
       <span class="pr-2"><Icon path={mdiAlert} /></span>
@@ -42,7 +55,7 @@
     </ul>
   </article>
 </div>
-<div class="box text-blue-600 bg-blue-200">
+      <div class="box text-blue-900 bg-blue-100">
   <article class="p-2">
     <p class="font-bold pb-1">Hallo und herzlich willkommen!</p>
     <p>Vielen Dank und viel Spaß.</p>
@@ -52,6 +65,7 @@
           <span on:click={() => (checked = !checked)} class="pl-2"
             >Ich bin mit den <a
               on:click|stopPropagation
+              class="text-blue-900 text-opacity-80 flex flex-wrap "
               href="#"
               ><span>Datenschutzbestimmungen</span><Icon path={mdiOpenInNew} size={0.7} /></a
             > einverstanden</span
@@ -61,6 +75,8 @@
           <Button disabled={!checked} title="Studie starten" on:click={start} />
         </div>
       </div>
+    </div>
+  </div>
 </div>
 
 <a
