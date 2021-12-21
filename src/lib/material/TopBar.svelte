e7f13a836081de9c08b138bbde52149b8b81781c
Ole Umlauft <ole.umlauft@protonmail.com>
2021-12-21 11:45:54 +0100

    Adds top bar

---
+++ src/lib/material/TopBar.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title;
+</script>
+
+<div class="flex flex-wrap content-center justify-between h-13 bg-blue-100 text-blue-900 px-1">
+  {#if $$slots.start}
+    <div>
+      <slot name="start" />
+    </div>
+  {/if}
+  <div class="px-2 flex flex-wrap content-center font-medium text-lg">{title}</div>
+  {#if $$slots.end}
+    <div>
+      <slot name="end" />
+    </div>
+  {/if}
+</div>
