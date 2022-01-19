44f990a3272f70187633ba16cd8dc75263ed0f25
Ole Umlauft <ole.umlauft@protonmail.com>
2022-01-19 18:27:15 +0100

    Add id to proband_data table

--- migrations/20211201163550_propand_data.cjs
+++ migrations/20211201163550_propand_data.cjs
@@ -1,7 +1,8 @@
 
 exports.up = function (knex) {
   return knex.schema.createTable('proband_data', function (table) {
-    table.string('token').primary();
+    table.increments('id').unsigned();
+    table.string('token').unique().notNullable();
     table.string('userAgent').notNullable();
     table.string('initScreenSize').notNullable();
     table.bigInteger('createdAt').notNullable();
