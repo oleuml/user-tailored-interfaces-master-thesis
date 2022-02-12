90c8cc4422f900cdc6eed27fd80f71a1674c31cf
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-12 15:49:12 +0100

    Add wrapper component for rounded top when active

---
+++ src/lib/components/Rounded.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let active: boolean = false;
+</script>
+
+<div
+  class:rounded-t-xl={active}
+  class:duration-100={!active}
+  class:duration-200={active}
+  class:overflow-hidden={active}
+  class="transition:rounded-t-xl h-full"
+>
+  <slot />
+</div>
