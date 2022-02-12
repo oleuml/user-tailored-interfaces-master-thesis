ca40332776c971e2ffa0498a0fc8134647e8d684
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-12 16:26:49 +0100

    Refactor MemberSlider component and add track handling

--- src/lib/components/ui3/MemberSlider.svelte
+++ src/lib/components/ui3/MemberSlider.svelte
@@ -3,6 +3,10 @@
   import MemberBadge from '$lib/components/MemberBadge.svelte';
   import Slider, { checked } from '$lib/components/ui3/Slider.svelte';
   import { mdiCheckboxBlankCircle, mdiCheckCircle } from '@mdi/js';
+  import { createEventDispatcher } from 'svelte';
+  import type { Action } from '$lib/stores/taskTracking';
+
+  const dispatcher = createEventDispatcher();
 
   export let title: string;
   export let score: number;
@@ -14,18 +18,30 @@
     throw new Error(`proposedScore must be a number: ${proposedScore}`);
   }
   if (proposedScore < 0 || proposedScore > 1) {
-    throw new Error(`proposedScore must be betweennumber or equals 0.0 and 1.0: ${proposedScore}`);
+    throw new Error(`proposedScore must be between or equals 0.0 and 1.0: ${proposedScore}`);
   }
   if (threshold < 0 || threshold > 1) {
-    throw new Error(`threshold must be betweennumber or equals 0.0 and 1.0: ${threshold}`);
+    throw new Error(`threshold must be between or equals 0.0 and 1.0: ${threshold}`);
   }
   if (score < 0 || score > 1) {
-    throw new Error(`score must be betweennumber or equals 0.0 and 1.0: ${score}`);
+    throw new Error(`score must be between or equals 0.0 and 1.0: ${score}`);
   }
+
+  const track = (action: Action, data?: any) => {
+    dispatcher('track', { action: action, data: data });
+  };
 </script>
 
 <div class="flex h-full">
-  <Slider bind:score bind:threshold>
+  <Slider
+    bind:score
+    bind:threshold
+    on:track={({ detail: { action, data } }) => {
+      data.position = 'slider';
+      data.member = title;
+      track(action, data);
+    }}
+  >
     <div class="flex flex-wrap h-full items-center gap-2 ml-2">
       <MemberBadge name={title} {color} badged={null} />
       {title}
@@ -41,6 +57,7 @@
       } else {
         score = 1.0;
       }
+      track('change-score', { score: score, member: title, memo: 'button-clicked' });
     }}
   >
     {#if !checked(threshold, score)}
