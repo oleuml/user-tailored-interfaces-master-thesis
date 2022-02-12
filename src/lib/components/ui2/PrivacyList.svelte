b629aee208d1c6f6d5cb29ecc1e86f66aff43394
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-12 16:25:37 +0100

    Refactor PrivacyList component and add track handling

--- src/lib/components/ui2/PrivacyList.svelte
+++ src/lib/components/ui2/PrivacyList.svelte
@@ -1,10 +1,15 @@
-<script>
+<script lang="ts">
+  import type { Member } from '$lib/members';
+  import type { Action } from '$lib/stores/taskTracking';
+
   import { mdiCheckCircle } from '@mdi/js';
   import Icon from 'mdi-svelte';
+  import { createEventDispatcher } from 'svelte';
   import MemberBadge from '../MemberBadge.svelte';
 
-  export let members;
-  export let tracking;
+  const dispatcher = createEventDispatcher();
+
+  export let members: Array<Member>;
   const alphabet = [
     'A',
     'B',
@@ -34,27 +39,24 @@
     'Z'
   ];
   $: members.sort((a, b) => a.name.split(' ')[1].localeCompare(b.name.split(' ')[1]));
+
+  const track = (action: Action, data?: any) => {
+    dispatcher('track', { action: action, data: data });
+  };
 </script>
 
 <div
   class="flex flex-wrap w-full justify-center"
   on:scroll={(event) => {
-    tracking.push({
-      t: Date.now(),
-      action: 'scroll',
-      pos: 'privacy_modal_list',
-      data: {
-        scrollHeight: event.target.scrollHeight,
-        scrollLeft: event.target.scrollLeft,
-        scrollLeftMax: event.target.scrollLeftMax,
-        scrollTop: event.target.scrollTop,
-        scrollTopMax: event.target.scrollTopMax,
-        scrollWidth: event.target.scrollWidth
-      }
-    });
+    let data = {
+      scrollHeight: event.target['scrollHeight'],
+      scrollTop: event.target['scrollTop'],
+      scrollTopMax: event.target['scrollTopMax']
+    };
+    track('scroll', data);
   }}
 >
-  <div class="w-5/6">
+  <div class="w-full">
     <div class="pt-3">
       <span class="text-xs text-gray-400">#☆</span>
       <div class="w-full h-0.25 bg-gray-100 mb-0.5" />
@@ -63,12 +65,11 @@
           class="flex flex-column justify-between items-center py2-0.5"
           on:click={() => {
             member.checked = !member.checked;
-            tracking.push({
-              t: Date.now(),
-              action: `member-checked-${member.checked}`,
-              pos: `privacy-modal-list`,
-              member: member.name
-            });
+            if (member.checked) {
+              track('select', { member: member.name });
+            } else {
+              track('deselect', { member: member.name });
+            }
           }}
         >
           <span class="flex flex-wrap items-center gap-2"
@@ -114,7 +115,14 @@
           {#each members.filter((x) => x.name.split(' ')[1][0] === char && !x.favorite) as member}
             <li
               class="flex flex-column justify-between items-center py-0.5"
-              on:click={() => (member.checked = !member.checked)}
+              on:click={() => {
+                member.checked = !member.checked;
+                if (member.checked) {
+                  track('select', { member: member.name });
+                } else {
+                  track('deselect', { member: member.name });
+                }
+              }}
             >
               <span class="flex flex-wrap items-center gap-2"
                 ><MemberBadge
@@ -154,10 +162,3 @@
     {/each}
   </div>
 </div>
-<!-- <div class="flex w-4">
-    <ul>
-      {#each ["☆", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] as char}
-        <li class="text-xs text-center text-gray-400">{char}</li>
-      {/each}
-    </ul>
-  </div> -->
