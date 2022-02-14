3669162d5f039496689363ef22a1615a01c707ae
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-14 19:45:34 +0100

    Change last migration

--- migrations/20220118130708_task_tracking.cjs
+++ migrations/20220118130708_task_tracking.cjs
@@ -3,9 +3,9 @@ exports.up = function (knex) {
   return knex.schema.createTable('task_tracking', function (table) {
     table.increments('id').primary().unsigned();
     table.integer('uid').notNullable();
+    table.integer('ui').notNullable();
     table.integer('taskid').notNullable();
     table.string('action').notNullable();
-    table.string('position').notNullable();
     table.text('data');
     table.bigInteger('timestamp').notNullable();
   })
