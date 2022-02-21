55bb7d2ae930aebc02e919b9e960cd67b2c9ecb8
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-21 23:16:55 +0100

    Implement new exercise types

--- src/lib/exercises.ts
+++ src/lib/exercises.ts
@@ -1,6 +1,13 @@
+import _ from 'lodash';
 import type { Member } from './members';
 
-export type Condition = 'all' | 'nobody' | 'select-one' | 'deselect-one' | 'selection-of-choice';
+export type Condition =
+  | 'all'
+  | 'nobody'
+  | 'select-one'
+  | 'deselect-one'
+  | 'selection-of-choice'
+  | 'select-only-one-of-group';
 
 export type Exercise = {
   id: number;
@@ -20,12 +27,54 @@ export const checkCondition = (condition: Condition, members: Member[], conditio
       return members.filter((m) => !m.checked).length === 0;
     case 'nobody':
       return members.filter((m) => m.checked).length === 0;
+    case 'select-only-one-of-group':
+      return (
+        members.find((m) => m.name === conditionBody.member).checked &&
+        members.filter((m) => m.group === conditionBody.group && m.checked).length === 1
+      );
     case 'select-one':
-      throw new Error(`Unimplemented condition check: ${condition}`);
+      return members.find((m) => m.name === conditionBody.member).checked;
     case 'deselect-one':
       return !members.find((m) => m.name === conditionBody.member).checked;
-    case 'selection-of-choice':
-      throw new Error(`Unimplemented condition check: ${condition}`);
+    case 'selection-of-choice': {
+      const groupKeys = Object.keys(conditionBody);
+      return groupKeys
+        .map((key): boolean => {
+          if (
+            conditionBody[key] &&
+            typeof conditionBody[key] === 'object' &&
+            conditionBody[key] instanceof Object
+          ) {
+            let selected: string[] = conditionBody[key].selectedMembers;
+            let deselected: string[] = conditionBody[key].deselectedMembers;
+            let intersection = _.intersection(selected, deselected);
+            if (intersection.length > 0) {
+              throw new Error(
+                `Same member(s) in selectedMembers and deselectedMembers: ${intersection}`
+              );
+            }
+            console.log(
+              members.filter(
+                (m) =>
+                  (selected.includes(m.name) && !m.checked) ||
+                  (deselected.includes(m.name) && m.checked)
+              )
+            );
+            return (
+              members.filter(
+                (m) =>
+                  (selected.includes(m.name) && !m.checked) ||
+                  (deselected.includes(m.name) && m.checked)
+              ).length === 0
+            );
+          } else if (conditionBody[key] === 'all') {
+            return members.filter((m) => m.group === key && !m.checked).length === 0;
+          } else if (conditionBody[key] === 'nobody') {
+            return members.filter((m) => m.group === key && m.checked).length === 0;
+          } else throw new Error('Wrong condition body format!');
+        })
+        .reduce((a, b) => a && b);
+    }
     default:
       throw new Error(`Unknown condition: ${condition}`);
   }
