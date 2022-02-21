fc11b32d7bff1a0c87f9fdc6a14d38a19eda0bde
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-21 22:39:04 +0100

    Fix full width of AppTopBar

--- src/lib/components/material/TopBar.svelte
+++ src/lib/components/material/TopBar.svelte
@@ -2,7 +2,9 @@
   export let title;
 </script>
 
-<div class="flex flex-wrap content-center justify-between h-13 bg-blue-100 text-blue-900 px-1">
+<div
+  class="flex flex-wrap content-center w-full justify-between h-13 bg-blue-100 text-blue-900 px-1"
+>
   {#if $$slots.start}
     <div>
       <slot name="start" />
