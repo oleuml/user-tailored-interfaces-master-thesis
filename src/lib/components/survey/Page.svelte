c5f78f7a1a79ff57f0841f29dd29b3c3a2e6532d
Ole Umlauft <ole.umlauft@protonmail.com>
2022-02-21 17:19:53 +0100

    Initalize answers with null and refactoring

--- src/lib/components/survey/Page.svelte
+++ src/lib/components/survey/Page.svelte
@@ -1,31 +1,39 @@
-<script context="module">
-  export function checkAllFulfilled(questions, answers) {
-    return questions
-      .map((q) => questionAnswered(q.type, answers[q.qid]))
-      .reduce((prev, curr) => prev && curr, true);
-  }
-</script>
-
 <script>
-  import Question, { questionAnswered } from '$lib/components/survey/Question.svelte';
+  import Question from '$lib/components/survey/Question.svelte';
+  import { questionAnswered } from '$lib/stores/answers';
 
   export let questions;
   export let answers;
 
   export let check;
+
+  let lastQuestions = [];
+
+  $: if (questions !== lastQuestions) {
+    lastQuestions = questions;
+    questions.forEach((question) => {
+      if (answers[`${question.id}`] === undefined) {
+        if (question.type === 'group-likert') {
+          answers[`${question.id}`] = question.answers.map((_) => null);
+        } else answers[`${question.id}`] = null;
+      }
+    });
+  }
 </script>
 
 <div class="space-y-2">
-  {#each questions as q}
-    <Question
-      question={q.question}
-      questionPrefix={q.questionPrefix}
-      type={q.type}
-      answers={q.answers}
-      legendType={q.legendType}
-      bind:answer={answers[q.qid]}
-      unfulfilledAlert={check && !questionAnswered(q.type, answers[q.qid])}
-      noStatement={q.noStatement}
-    />
-  {/each}
+  {#if answers && Object.keys(answers).length > 0 && Object.getPrototypeOf(answers) === Object.prototype}
+    {#each questions as q}
+      <Question
+        question={q.question}
+        questionPrefix={q.questionPrefix}
+        type={q.type}
+        answers={q.answers}
+        legendType={q.legendType}
+        bind:answer={answers[q.id]}
+        unfulfilledAlert={check && !questionAnswered(q.type, answers[q.id])}
+        noStatement={q.noStatement}
+      />
+    {/each}
+  {/if}
 </div>
