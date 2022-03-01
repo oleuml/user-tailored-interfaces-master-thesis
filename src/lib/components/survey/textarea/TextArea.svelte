25bd08ee5206727293ab98cb22c97deccf4e8a68
Ole Umlauft <ole.umlauft@protonmail.com>
2022-03-01 10:02:44 +0100

    Fix text selection

--- src/lib/components/survey/textarea/TextArea.svelte
+++ src/lib/components/survey/textarea/TextArea.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <textarea
-  class="w-full h-24 outline-none focus:ring-2 ring-blue-900 rounded-md p-2 text-sm"
+  class="w-full h-24 outline-none focus:ring-2 ring-blue-900 rounded-md p-2 text-sm select-text"
   bind:value={othersTextValue}
   on:input={() => {
     answer = { text: othersTextValue, index: 0 };
